572598127bc2dfaafe610673d37aa259aa63752e
deepx-jbj <jbj@deepx.ai>
2023-03-20 10:01:30 +0900

    add_ch9

---
+++ fluent_python_2nd/2_functions_as_objects/registration.py
@@ -0,0 +1,29 @@
+# 9_Decorators_and_Closures 용 python 파일
+
+registry = []
+
+def register(func):
+    print(f'running register({func})')
+    registry.append(func)
+    return func
+
+@register
+def f1():
+    print('running f1()')
+    
+@register
+def f2():
+    print('running f2()')
+    
+def f3():
+    print('running f3()')
+
+def main():
+    print('running main()')
+    print('registry ->', registry)
+    f1()
+    f2()
+    f3()
+    
+if __name__ == '__main__':
+    main()
